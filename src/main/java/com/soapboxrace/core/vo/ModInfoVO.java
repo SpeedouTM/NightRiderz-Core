152fd57abd909cacefc01ddd52a3be945745a3f3
SpeedouTM <34949863+SpeedouTM@users.noreply.github.com>
2019-08-22 14:27:25 +0200

    New modmanager system (testing)

---
+++ src/main/java/com/soapboxrace/core/vo/ModInfoVO.java
@@ -0,0 +1,24 @@
+package com.soapboxrace.core.vo;
+
+public class ModInfoVO {
+
+    private String basePath;
+
+    private String serverID;
+
+    public String getBasePath() {
+        return basePath;
+    }
+
+    public void setBasePath(String basePath) {
+        this.basePath = basePath;
+    }
+
+    public String getServerID() {
+        return serverID;
+    }
+
+    public void setServerID(String serverID) {
+        this.serverID = serverID;
+    }
+}
